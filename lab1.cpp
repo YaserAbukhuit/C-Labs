0990f31400829982119be604864f66f2656a4aca
YaserAbukhuit <yaser0abukhuit0@gmail.com>
2024-11-03 01:18:10 +0300

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+//Variables: Declaration & Initialization 
+// Problem : 
+// Declare and initialize variables based on the following requirements:
+
+// Write declarations for double variables, x and y, initialized to 3.1 and 5.667, respectively.
+// Write declarations for char variables, ch1 and ch2, initialized to 'a' and 'Z', respectively.
+// Write declarations for string variables, str1 and str2, initialized to "Omar" and "Laila", respectively. 
+
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    
+    double x=3.1 ,y=5.667;
+    char ch1='a' ,ch2='Z';
+    string str1="Omar",str2="Laila"; 
+    
+    
+    return 0;
+}
